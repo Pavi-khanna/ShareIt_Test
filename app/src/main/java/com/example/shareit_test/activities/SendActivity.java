396d52d8aefc8f328c4a8fd8f07bc0620447c67d
Yashasvi Asthana <yashasvi.asthana@gmail.com>
2020-07-07 22:18:31 -0400

    Broadcast Receiver and List Frag added TODO: Create custom list with row view

--- app/src/main/java/com/example/shareit_test/activities/SendActivity.java
+++ app/src/main/java/com/example/shareit_test/activities/SendActivity.java
@@ -2,12 +2,16 @@ package com.example.shareit_test.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.shareit_test.R;
@@ -17,13 +21,17 @@ import java.util.ArrayList;
 public class SendActivity extends AppCompatActivity {
 
     ListView senderListView;
+    Button createhotspot;
+    WifiP2pManager.Channel channel;
+    WifiP2pManager manager;
+    private final IntentFilter intentFilter = new IntentFilter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
-
-        senderListView = findViewById(R.id.list_send);
+        createhotspot = findViewById(R.id.hotspot);
+        senderListView = findViewById(R.id.avail_devices_list);
         ArrayList<String> senderArrayList = new ArrayList<>();
         senderArrayList.add("IP 1");
         senderArrayList.add("IP 2");
@@ -40,5 +48,17 @@ public class SendActivity extends AppCompatActivity {
                 startActivity(browseFilesIntent);
             }
         });
+
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+
+
+        createhotspot.setVisibility(View.INVISIBLE);
+        createhotspot.postDelayed(new Runnable() {
+            public void run() {
+                createhotspot.setVisibility(View.VISIBLE);
+            }
+        }, 5000);
     }
 }
