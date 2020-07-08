b1c9337d6f6f051bb2a6877b18f05baa33d340f3
Pavi <pavkhann@amazon.com>
2020-07-09 02:54:02 +0530

    Added File Browsing

--- app/src/main/java/com/example/shareit_test/activities/BrowseFilesActivity.java
+++ app/src/main/java/com/example/shareit_test/activities/BrowseFilesActivity.java
@@ -8,16 +8,31 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.Nullable;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.example.shareit_test.activities.ui.main.SectionsPagerAdapter;
 
 public class BrowseFilesActivity extends AppCompatActivity {
+
+    String path;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1001:
+                if (resultCode == RESULT_OK) {
+                    path = data.getData().getPath();
+                }
+                break;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +51,17 @@ public class BrowseFilesActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1 = new Intent(BrowseFilesActivity.this, SendProcessingActivity.class);
-                startActivity(intent1);
-                //Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
-                //        .setAction("Action", null).show();
+                //Intent intent1 = new Intent(BrowseFilesActivity.this, SendProcessingActivity.class);
+                //startActivity(intent1);
+
+                Intent filesOpener = new Intent(Intent.ACTION_GET_CONTENT);
+                //filesOpener.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                //filesOpener.addCategory(Intent.CATEGORY_OPENABLE);
+                filesOpener.setType("*/*");
+                startActivityForResult(filesOpener, 1001);
+
+                Snackbar.make(view, path, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
